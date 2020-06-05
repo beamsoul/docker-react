922dd9b3853487cc74d4c3efc23213a71bcbaa1b
Janna <janna@Jannas-MacBook-Air.local>
2020-06-04 21:05:52 -0500

    dfgdfgtyujt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Janna edit <code>src/App.js</code> you are the best.
         </p>
         <a
           className="App-link"
